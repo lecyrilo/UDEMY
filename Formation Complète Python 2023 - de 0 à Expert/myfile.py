4481de7119f804f64a76e425273408ff8a3facd3
lecyrilo <letatus2021@gmail.com>
2023-06-02 08:46:36 +0200

    3rd-020623

--- Formation Complète Python 2023 - de 0 à Expert/myfile.py
+++ Formation Complète Python 2023 - de 0 à Expert/myfile.py
@@ -4,7 +4,7 @@ import os
 number_1 = random.randint(0,5)
 number_2 = random.randint(0,5)
 if number_1 == number_2:
-    print(f"{number_1} et {number_2}are equal")
+    print(f"{number_1} and {number_2}are equal")
 elif number_1 > number_2:
     print(f"{number_1} are bigger than {number_2}")
 elif number_1 < number_2:
@@ -12,3 +12,20 @@ elif number_1 < number_2:
 
 r = random.randrange(0,100,5)
 print(r)
+
+print(dir(random))
+help(random.random)
+
+x = 10
+#y = "f5fg"
+#print(x/y)
+
+try:
+    print(x/y)
+except ZeroDivisionError:
+    print("division with o impossible")
+except TypeError:
+    print("y type ist not correct")
+except NameError:
+    print("y ist defined")
+    
